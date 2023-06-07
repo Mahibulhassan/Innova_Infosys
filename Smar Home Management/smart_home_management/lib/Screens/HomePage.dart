6a7ada2dcdf385333cea6d1964e6c47abe6e7d4d
shadbinbari420 <shadbinbari753@gmail.com>
2023-06-07 16:30:18 +0600

    Home Page Responsive done

--- Smar Home Management/smart_home_management/lib/Screens/HomePage.dart
+++ Smar Home Management/smart_home_management/lib/Screens/HomePage.dart
@@ -16,179 +16,178 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-        height: size.height,
-        width: size.width,
+      body: Center(
         child: Stack(
           children: [
-            Stack(
+            Column(
               children: [
                 Container(
-                  height: size.height/2.5,
-                  width: size.width,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                  ),
-                ),
-                Container(
-                  height: size.height /2.52,
-                  width: size.width,
+                  height:size.height*0.431 ,
                   decoration: BoxDecoration(
                     color: AppColors.main_colour,
+                    borderRadius: BorderRadius.only(bottomLeft: Radius.circular(50)),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: AppColors.main_colour,
                       borderRadius: BorderRadius.only(bottomLeft: Radius.circular(50)),
-                  ),
-                  child: SafeArea(
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  showText("Good Morning,", 30, Colors.white),
-                                  Text("Kimberly Masterangelo",),
-                                ],
-                              ),
-                              Container(
-                                height: size.width *0.1,
-                                width: size.width *0.1,
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.all(Radius.circular(50)),
-                                ),
-                                  child: Icon(Icons.notification_add)),
-                            ],
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
-                          child: Container(
-                            height: size.height * 0.23,
-                            width: size.width,
-                            decoration: BoxDecoration(
-                              color: Colors.white70,
-                              borderRadius: BorderRadius.all(Radius.circular(50)),
-                            ),
-
-                            //Top Order View Implemented Here.....
-                            child: Padding(
-                              padding: const EdgeInsets.all(16.0),
-                              child: Column(
-                                children: [
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                    children: [
-                                      Container(
-                                        height: 70,
-                                        width: 70,
-                                        child: Image(
-                                            image: AssetImage("assets/weather.png")),
-                                      ),
-                                      Column(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        children: [
-                                          Text("May 16,2023 10:5 am"),
-                                          showText("Cloudy", 20, Colors.black),
-                                          Text("Jakarta, Indonesia")
-                                        ],
-                                      ),
-                                      Container(
-                                        height: 70,
-                                        width: 70,
-                                        child: Text("19 C",style: TextStyle(fontSize: 30),),
-                                      ),
-                                    ],
-                                  ),
-                                  Divider(color: Colors.black54,),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                    children: [
-                                      HomeTopView(Icons.water_drop,"97 %","Humadit"),
-                                      HomeTopView(Icons.remove_red_eye,"7 Km","Visibility"),
-                                      HomeTopView(Icons.wind_power_outlined,"3 km/h","NE Wind")
-                                    ],
-                                  )
-                                ],
-                              ),
-                            ),
-                          ),
-                        )
-                      ],
-                    )
-                  )
-                ),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Container(
-                    width: size.width,
-                    height: size.height/ 2,
-                    decoration: BoxDecoration(
-                      color: AppColors.main_colour
                     ),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Container(
-                    width: size.width,
-                    height: size.height/ 2,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(topRight: Radius.circular(50)),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                            showText("Rooms", 20, Colors.black),
-                            showText("See All", 20, Colors.blueAccent),
-                          ],),
-                          SizedBox(height: 5,),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              HomeMiddleView(size,"19 C","Living Room","5 Device","assets/sufa.png"),
-                              HomeMiddleView(size,"12 C","BedRoom","8 Device","assets/sufa.png")
-                            ],
-                          ),
-                          SizedBox(height: 5,),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Row(
-                                children: [
-                                  showText("Active", 20, Colors.black),
-                                  SizedBox(width: 5,),
-                                  Container(
-                                    height: 20,
-                                    width: 20,
-                                    color: AppColors.main_colour,
-                                    child: Center(child: Text("6",style: TextStyle(color: Colors.white),)),
-                                  )
-                                ],
-                              ),
-                              showText("See All", 20, Colors.blueAccent)
-                            ],),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              HomeLowerView(size,"Temparature","19 C","Ac","Living Room","assets/ac.png"),
-                              HomeLowerView(size,"Colour","White","Lamp","Dining Room","assets/ac.png"),
-                            ],
-                          ),
-                        ],
+                    child: Container(
+                      height:size.height*0.49 ,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(topRight: Radius.circular(50)),
                       ),
                     ),
                   ),
                 ),
               ],
             ),
+            SafeArea(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              showText("Good Morning,", 30, Colors.white),
+                              Text("Kimberly Masterangelo",),
+                            ],
+                          ),
+                          Container(
+                              height: size.width *0.1,
+                              width: size.width *0.1,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.all(Radius.circular(50)),
+                              ),
+                              child: Icon(Icons.notification_add)),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+                      child: Container(
+                        height: size.height * 0.25,
+                        width: size.width,
+                        decoration: BoxDecoration(
+                          color: Colors.white70,
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                        ),
+                        //Top Order View Implemented Here.....
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            children: [
+                              Container(
+                                height: size.height*0.08,
+                                width: size.width*01,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    Container(
+                                      height: size.height*0.08,
+                                      width: size.width*0.15,
+                                      child: Image(
+                                          image: AssetImage("assets/weather.png")),
+                                    ),
+                                    Container(
+                                      height: size.height*0.09,
+                                      width: size.width*0.40,
+                                      child: FittedBox(
+                                        child: Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: [
+                                            Text("May 16,2023 10:5 am"),
+                                            showText("Cloudy", 20, Colors.black),
+                                            Text("Jakarta, Indonesia")
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                    Container(
+                                      height: size.height*0.20,
+                                      width: size.width*0.15,
+                                      child: Text("19 C",style: TextStyle(fontSize: 30),),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Divider(color: Colors.black54,),
+                              Container(
+                                height: size.height*0.08,
+                                width: size.width*01,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    HomeTopView(size,Icons.water_drop,"97 %","Humadit"),
+                                    HomeTopView(size,Icons.remove_red_eye,"7 Km","Visibility"),
+                                    HomeTopView(size,Icons.wind_power_outlined,"3 km/h","NE Wind")
+                                  ],
+                                ),
+                              ),
+
+
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: size.height*0.05,),
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          showText("Rooms", 20, Colors.black),
+                          showText("See All", 20, Colors.blueAccent),
+                        ],),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        HomeMiddleView(size,"19 C","Living Room","5 Device","assets/sufa.png"),
+                        HomeMiddleView(size,"12 C","BedRoom","8 Device","assets/sufa.png")
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              showText("Active", 20, Colors.black),
+                              SizedBox(width: 5,),
+                              Container(
+                                height: size.height*0.025,
+                                width: size.width*0.05,
+                                color: AppColors.main_colour,
+                                child: Center(child: Text("6",style: TextStyle(color: Colors.white),)),
+                              )
+                            ],
+                          ),
+                          showText("See All", 20, Colors.blueAccent)
+                        ],),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        HomeLowerView(size,"Temparature","19 C","Ac","Living Room","assets/ac.png"),
+                        HomeLowerView(size,"Colour","White","Lamp","Dining Room","assets/ac.png"),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
--- Smar Home Management/smart_home_management/lib/widgets/HomePageWidgets/Allwidgets.dart
+++ Smar Home Management/smart_home_management/lib/widgets/HomePageWidgets/Allwidgets.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import '../../uility/AppColors.dart';
 
-Widget HomeTopView(var icon ,String data, String name){
+Widget HomeTopView(Size size,var icon ,String data, String name){
   return Container(
-    height: 70,
-    width: 110,
+    height:size.height*0.20,
+    width: size.width*0.26,
     decoration: BoxDecoration(
       color: Colors.white12,
       borderRadius: BorderRadius.all(Radius.circular(20)),
     ),
     child: Padding(
-      padding: EdgeInsets.all(6.0),
+      padding: EdgeInsets.all(4.0),
       child: Column(
         children: [
           Row(
@@ -18,8 +18,8 @@ Widget HomeTopView(var icon ,String data, String name){
             children: [
               Container(
                 margin: EdgeInsets.only(left: 2),
-                height: 30,
-                width: 30,
+                height: size.height*0.04,
+                width: size.width*0.08,
                 decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.all(Radius.circular(50))
@@ -38,99 +38,108 @@ Widget HomeTopView(var icon ,String data, String name){
 }
 
 
-Widget HomeMiddleView(var size ,String tempar,String name,String device,String image ){
-
-  return Container(
-    height: size.height*0.23,
-    width: size.width*0.45,
-    decoration: BoxDecoration(
-      color: AppColors.shasow_colour,
-      borderRadius: BorderRadius.all(Radius.circular(20)),
-    ),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Container(
-            height: 20,
-            width: 40,
-            child: Text(tempar,style: TextStyle(color: Colors.white),),
-            color: AppColors.main_colour,
-          ),
+Widget HomeMiddleView(Size size ,String tempar,String name,String device,String image ){
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Container(
+      height: size.height*0.23,
+      width: size.width*0.45,
+      decoration: BoxDecoration(
+        color: AppColors.shasow_colour,
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+      ),
+      child: FittedBox(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                height: size.height*0.03,
+                width: size.width*0.08,
+                child: Text(tempar,style: TextStyle(color: Colors.white),),
+                color: AppColors.main_colour,
+              ),
+            ),
+            Center(
+              child: Container(
+                height: size.height*0.10,
+                width: size.width*0.40,
+                child: FittedBox(
+                    child:Image(
+                        image: AssetImage(image)) ),
+              ),
+            ),
+            Text(name,style: TextStyle(color: Colors.black,fontSize: 18),),
+            Text(device,style: TextStyle(color: Colors.black),),
+          ],
         ),
-        Center(
-          child: Container(
-            height: 100,
-            width: 100,
-            child: FittedBox(
-                child:Image(
-                    image: AssetImage(image)) ),
-          ),
-        ),
-        Center(child: Text(name,style: TextStyle(color: Colors.black,fontSize: 18),)),
-        Center(child: Text(device,style: TextStyle(color: Colors.black),)),
-      ],
+      ),
     ),
   );
 }
 
 
 Widget HomeLowerView(var size,String name,String subname,String lname,String lsubname,String image){
-  return Container(
-    height: size.height*0.16,
-    width: size.width*0.45,
-    decoration: BoxDecoration(
-      color: AppColors.shasow_chocklet,
-      borderRadius: BorderRadius.all(Radius.circular(20)),
-    ),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                height: 70,
-                width:70,
-                child:  Image(
-                    image: AssetImage(image)),
-              ),
-              Column(
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Container(
+      height: size.height*0.13,
+      width: size.width*0.45,
+      decoration: BoxDecoration(
+        color: AppColors.shasow_chocklet,
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+      ),
+      child: FittedBox(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Container(
-                    child: Text(name,style: TextStyle(color: Colors.white),),
+                    height: size.height*0.05,
+                    width: size.width*0.15,
+                    child:  Image(
+                        image: AssetImage(image)),
                   ),
-                  Text(subname,style: TextStyle(color: Colors.white),),
+                  Column(
+                    children: [
+                      Container(
+                        child: Text(name,style: TextStyle(color: Colors.white),),
+                      ),
+                      Text(subname,style: TextStyle(color: Colors.white),),
+                    ],
+                  ),
                 ],
               ),
-            ],
-          ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(lname,style: TextStyle(color: Colors.white,fontSize: 20),),
-                Text(lsubname,style: TextStyle(color: Colors.white,fontSize: 14),),
-              ],
             ),
             Padding(
-              padding: const EdgeInsets.all(7.0),
-              child: Container(
-                height: 40,
-                width: 40,
-                child: Image(
-                    image: AssetImage("assets/switch.png")),
+              padding: const EdgeInsets.only(left: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(lname,style: TextStyle(color: Colors.white,fontSize: 20),),
+                      Text(lsubname,style: TextStyle(color: Colors.white,fontSize: 14),),
+                    ],
+                  ),
+                  Container(
+                    height: size.height*0.05,
+                    width: size.width*0.20,
+                    child: Image(
+                        image: AssetImage("assets/switch.png")),
+                  )
+                ],
               ),
             )
           ],
-        )
-      ],
+        ),
+      ),
     ),
   );
 }
