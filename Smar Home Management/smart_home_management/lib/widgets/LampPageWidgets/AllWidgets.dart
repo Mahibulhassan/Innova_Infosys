7626281cbc95f67fcc5c4e9816ad07402ed78bb6
shadbinbari420 <shadbinbari753@gmail.com>
2023-06-07 18:06:21 +0600

    Lamp Page Dinamic Done

--- Smar Home Management/smart_home_management/lib/widgets/LampPageWidgets/AllWidgets.dart
+++ Smar Home Management/smart_home_management/lib/widgets/LampPageWidgets/AllWidgets.dart
@@ -2,46 +2,45 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 
-Widget LampMiddleView(){
+Widget LampMiddleView(Size size){
 
-  return Padding(
-    padding: const EdgeInsets.all(16.0),
-    child: Container(
-      child:  Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text("Usages",style: TextStyle(color: Colors.white,fontSize: 15),),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text("Use Today",style: TextStyle(color: Colors.white,fontSize: 15),),
-              Text("50 Watt",style: TextStyle(color: Colors.white,fontSize: 15),),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text("Use Week",style: TextStyle(color: Colors.white,fontSize: 15),),
-              Text("500 Watt",style: TextStyle(color: Colors.white,fontSize: 15),),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text("Use Month",style: TextStyle(color: Colors.white,fontSize: 15),),
-              Text("5000 Watt",style: TextStyle(color: Colors.white,fontSize: 15),),
-            ],
-          ),
-        ],
-      ),
+  return Container(
+    height: size.height*0.11,
+    width: size.width,
+    child:  Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("Usages",style: TextStyle(color: Colors.white,fontSize: 15),),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("Use Today",style: TextStyle(color: Colors.white,fontSize: 15),),
+            Text("50 Watt",style: TextStyle(color: Colors.white,fontSize: 15),),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("Use Week",style: TextStyle(color: Colors.white,fontSize: 15),),
+            Text("500 Watt",style: TextStyle(color: Colors.white,fontSize: 15),),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("Use Month",style: TextStyle(color: Colors.white,fontSize: 15),),
+            Text("5000 Watt",style: TextStyle(color: Colors.white,fontSize: 15),),
+          ],
+        ),
+      ],
     ),
   );
 }
 
-Widget LampLowerView(var size,String heading,String subtext, String icon,String stime,String etime){
+Widget LampLowerView(Size size,String heading,String subtext, String icon,String stime,String etime){
   return Container(
     width: size.width,
-    height: size.height*0.148,
+    height: size.height*0.16,
     decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.all(Radius.circular(20))
@@ -60,8 +59,8 @@ Widget LampLowerView(var size,String heading,String subtext, String icon,String 
                   Text(subtext,style: TextStyle(fontSize: 18,color: Colors.black),),
                 ],),
               Container(
-                height: 60,
-                width: 60,
+                height: size.height*0.06,
+                width: size.width*0.09,
                 child: Image(
                     image: AssetImage(icon)),
               )
@@ -70,8 +69,8 @@ Widget LampLowerView(var size,String heading,String subtext, String icon,String 
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Container(
-                  height: 50,
-                  width: 50,
+                  height: size.height*0.07,
+                  width: size.width*0.09,
                   child: FittedBox(child: Icon(Icons.lightbulb))),
               Column(
                 children: [
