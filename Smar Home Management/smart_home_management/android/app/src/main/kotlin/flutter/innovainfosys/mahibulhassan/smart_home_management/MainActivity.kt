80857500ae9bd105497b8624882be70346e59606
Mahibul Hassan <mahibulhassan@Mahibuls-MacBook-Pro.local>
2023-07-29 14:52:47 +0600

    Final Version has been done

--- Smar Home Management/smart_home_management/android/app/src/main/kotlin/flutter/innovainfosys/mahibulhassan/smart_home_management/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package flutter.innovainfosys.mahibulhassan.smart_home_management
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Smar Home Management/smart_home_management/lib/uility/BaseApp.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:ffi';
-
-import 'package:flutter/cupertino.dart';
-
-Widget showText(String text,double fontSize,Color color){
-
-  return Text(text,style: TextStyle(fontSize: fontSize,color: color),);
-}
